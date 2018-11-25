0fb1db52d6bcb539c5b4b987f89e400c27f0bbfc
Steinger Marcel <github@steinger.ch>
2018-11-25 23:22:54 +0100

    Add tooltip for dates

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -19,7 +19,7 @@
         @foreach( $activities as $activity )
             <tr>
               <td>{{ $activity->name }}</td>
-              <td>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $activity->created_at)->diffForHumans() }}</td>
+              <td><span data-tooltip class="tooltip-1" tabindex="2" title="{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $activity->created_at)->formatLocalized('%e. %B %Y %H:%M')}}">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $activity->created_at)->diffForHumans() }}</span></td>
               <td>
                 <a class="hollow button" href="{{ route('delete', ['activity_id' => $activity->id ]) }}">{{__('Delete')}}</a>
               </td>
